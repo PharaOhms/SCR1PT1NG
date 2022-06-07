cfad35bbdb0f1fb6ebf85868ce83e9772ac8f252
PH4R4 <98988642+PharaOhms@users.noreply.github.com>
2022-06-07 21:33:27 +0200

    Create hexaConverter.py

---
+++ Python/hexaConverter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+#hexadecimalConverter
+
+import time
+
+def converter():
+    hex= input("\nInput hexadecimal number >> ")
+    decode= int(hex, 16)
+    print("\nConvert {} in process".format(hex))
+    time.sleep(1)
+    print ("\n [*] Port {} [*] \n".format(decode))
+
+converter()
+
+def add():
+    while True:
+        add=input("Do you want add more values? [y] or [n] >> ")
+        if add != "y" and add != "n":
+            print("\nThe answer is not valid, please choice [y] or [n]\n")
+        elif add == "y":
+            converter()
+        elif add == "n":
+            print ("Killing Process, byeee!\n")
+            break
+add()
