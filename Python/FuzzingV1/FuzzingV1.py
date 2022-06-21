32a6d1178abadc08a3db328dcabb6330fad91178
PH4R4 <98988642+PharaOhms@users.noreply.github.com>
2022-06-21 22:28:33 +0200

    Update FuzzingV1-1.py

--- Python/FuzzingV1/FuzzingV1.py
+++ Python/FuzzingV1/FuzzingV1.py
@@ -3,60 +3,159 @@ import time
 
 time.sleep(1)
 
-print("\nWelcome to Fuzzing v1.0 By @pharaohms.\n")
+print("\nWelcome to Fuzzing v1.1 By @pharaohms.\n")
 print("Requirements:\n"
 "\n[+]Wfuzz\n"
 "[+]Gobuster\n")
 
+
+
+##FUZZING WITH DIRECTORY-LIST-LOWERCASE-2.3-SMALL.TXT
+def DirSmall():
+
+    wfuzz = "wfuzz -c -w /usr/share/wordlists/dirbuster/directory-list-lowercase-2.3-small.txt --hc 404 "
+    print("\n[+] Protocol")
+    targetSSL = input(
+    "\n[1] HTTP"
+    "\n[2] HTTPS\n"
+    "\n>> ")
+
+    targetIP = input("\n[+] IP Target > ")
+    targetPort = input("[+] Port Target > ")
+    fuzz = "/FUZZ 2>/dev/null"
+
+    if targetSSL == "1":
+        url = "http://{}:{}".format(targetIP,targetPort)
+        command = (wfuzz + url + fuzz)
+        print ("\nFuzzing Directories {}:{}\n".format(targetIP,targetPort))
+        InvokeCommand = os.system(command)
+
+    elif targetSSL ==  "2":
+        url = "https://{}:{}".format(targetIP,targetPort)
+        command = (wfuzz + url + fuzz)
+        print("\nFuzzing directories {}:{}".format(targetIP,targetPort))
+        InvokeCommand = os.system(command)
+
  
+
 ##FUZZING WITH DIRECTORY-LIST-LOWERCASE-2.3-MEDIUM.TXT
 def DirMedium():
+
     wfuzz = "wfuzz -c -w /usr/share/wordlists/dirbuster/directory-list-lowercase-2.3-medium.txt --hc 404 "
-    target = input("\nIP Target > ")
+    print("\n[+] Protocol")
+    targetSSL = input(
+    "\n[1] HTTP"
+    "\n[2] HTTPS\n"
+    "\n>> ")
+
+    targetIP = input("\n[+] IP Target > ")
+    targetPort = input("[+] Port Target > ")
     fuzz = "/FUZZ 2>/dev/null"
-    command = (wfuzz + target + fuzz)
-    print ("\nFuzzing Directories {} \n".format(target))
-    InvokeCommand = os.system(command)
+
+    if targetSSL == "1":
+        url = "http://{}:{}".format(targetIP,targetPort)
+        command = (wfuzz + url + fuzz)
+        print ("\nFuzzing Directories {}:{}\n".format(targetIP,targetPort))
+        InvokeCommand = os.system(command)
+
+    elif targetSSL ==  "2":
+        url = "https://{}:{}".format(targetIP,targetPort)
+        command = (wfuzz + url + fuzz)
+        print("\nFuzzing directories {}:{}".format(targetIP,targetPort))
+        InvokeCommand = os.system(command)
+
+    
     
 #DirMedium()
 
 ##FUZZING WITH BIG.TXT
 def DirBig():
+
     wfuzz = "wfuzz -c -w /usr/share/wordlists/dirb/big.txt --hc 404 " 
-    target = input("\nIP Target > ")
+    print("\n[+] Protocol")
+    targetSSL = input(
+    "\n[1] HTTP"
+    "\n[2] HTTPS\n"
+    "\n>> ")
+
+    targetIP = input("\n[+] IP Target > ")
+    targetPort = input("[+] Port Target > ")
     fuzz = "/FUZZ 2>/dev/null"
-    command = (wfuzz + target + fuzz)
-    print ("\nFuzzing Directories {} \n".format(target))
-    InvokeCommand = os.system(command)
+
+    if targetSSL == "1":
+        url = "http://{}:{}".format(targetIP,targetPort)
+        command = (wfuzz + url + fuzz)
+        print ("\nFuzzing Directories {}:{}\n".format(targetIP,targetPort))
+        InvokeCommand = os.system(command)
+
+    elif targetSSL ==  "2":
+        url = "https://{}:{}".format(targetIP,targetPort)
+        command = (wfuzz + url + fuzz)
+        print("\nFuzzing directories {}:{}".format(targetIP,targetPort))
+        InvokeCommand = os.system(command)
 
 #DirBig()
 
 
 ##FUZZING WITH EXTENSIONS-BIG.TXT
 def BigExtensions():
+
     wfuzz = "gobuster dir -w /usr/share/wordlists/dirb/big.txt -u "
-    target = input("\nIP Target > ")
+    print("\n[+] Protocol")
+    targetSSL = input(
+    "\n[1] HTTP"
+    "\n[2] HTTPS\n"
+    "\n>> ")
+
+    targetIP = input("\n[+] IP Target > ")
+    targetPort = input("[+] Port Target > ")
     fuzz = " -x php,html,js,txt 2>/dev/null"
-    command = (wfuzz + target + fuzz)
-    print ("\nFuzzing Extensions {} \n".format(target))
-    InvokeCommand = os.system(command)
+
+    if targetSSL == "1":
+        url = "http://{}:{}/".format(targetIP,targetPort)
+        command = (wfuzz + url + fuzz)
+        print ("\nFuzzing Extensions {}:{}\n".format(targetIP,targetPort))
+        InvokeCommand = os.system(command)
+
+    elif targetSSL ==  "2":
+        url = "https://{}:{}/".format(targetIP,targetPort)
+        command = (wfuzz + url + fuzz)
+        print("\nFuzzing Extensions {}:{}".format(targetIP,targetPort))
+        InvokeCommand = os.system(command)
+
 
 #BigExtensions()
 
 ##FUZZING SUBDOMAINS WITH subdomains-top1million-20000.txt
 def SubDomains():
     wfuzz = "wfuzz -c -w /usr/share/SecLists/Discovery/DNS/subdomains-top1million-20000.txt --hc 200,400,404 -t 50 "
-    target = input("\nIP Target > ")
-    host = ('-H "Host: FUZZ.{}"'.format(target))
-    url = "-u http://{}".format(target)
-    command = (wfuzz + host + url)
-    print("\nFuzzing SubDomains\n")
-    InvokeCommand = os.system(command)
+    print("\n[+] Protocol")
+    targetSSL = input(
+    "\n[1] HTTP"
+    "\n[2] HTTPS\n"
+    "\n>> ")
 
-#SubDomains()
+    targetIP = input("\n[+] IP Target > ")
+    targetPort = input("[+] Port Target > ")
+
+    
+    host = ('-H "Host: FUZZ.{}:{}"'.format(targetIP,targetPort))
+    
+    if targetSSL == "1":
+        url = "-u http://{}:{}".format(targetIP,targetPort)
+        command = (wfuzz + host + url)
+        print ("\nFuzzing SubDomains {}:{}\n".format(targetIP,targetPort))
+        InvokeCommand = os.system(command)
+
+    elif targetSSL ==  "2":
+        url = "-u https://{}:{}".format(targetIP,targetPort)
+        command = (wfuzz + host + url)
+        print("\nFuzzing SubDomains {}:{}".format(targetIP,targetPort))
+        InvokeCommand = os.system(command)
+
+
 
 ##CALL START FUNCTION
-##IF YOU CHOICE [0] ALL, YOU CAN PASS AT THE NEXT STEP WITH CTRL + C##
 def startFuzz():
     time.sleep(1)
     startInput = input("\nInsert Fuzzing Index: \n"
@@ -69,13 +168,16 @@ def startFuzz():
 
     if startInput == "1":
         Wordlists = input("\nInsert Wordlist Index: \n"
-        "\n[1] directory-list-lowercase-2.3-medium.txt\n"
+        "\n[0] small.txt\n"
+        "\n[1] medium.txt\n"
         "\n[2] big.txt\n"
         "\n>> ")
         if Wordlists == "1":
             return DirMedium()
         elif Wordlists == "2":
             return DirBig()
+        elif Wordlists == "0":
+            return DirSmall()
     elif startInput == "2":
         return SubDomains()
     elif startInput == "3":
@@ -85,6 +187,8 @@ def startFuzz():
     elif startInput == "Q" or startInput == "q":
         time.sleep(1)
         print("\n[+]Killing Process, Byee! \n")
+    else:
+        print("\n[+] Usage: 0, 1, 2, 3 or Q/q for exit.\n")
+        return startFuzz()
         
 startFuzz()
-
