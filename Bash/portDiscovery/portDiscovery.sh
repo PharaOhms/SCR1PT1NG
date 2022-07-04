814f21bb1f7a9c0d5cb0bb2844df01a6a0312ac3
@pharaohms <98988642+PharaOhms@users.noreply.github.com>
2022-07-04 17:51:35 +0200

    Create portDiscovery.sh

---
+++ Bash/portDiscovery/portDiscovery.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+function ctrl_c(){
+	echo -e "\n[!]Exit"
+	exit 1
+}
+
+function portDiscovery(){
+	trap ctrl_c INT
+
+	echo -e "\n"
+	echo -e "[*]Host -> " | tr -d '\n'
+	read host
+
+	echo -e "[+]Port Discovery"
+
+	for port in $(seq 1 2000); do
+	        bash -c "echo ' ' > /dev/tcp/$host/$port" 2>/dev/null && echo -e "\t[*] Port $port -> Open"  &
+	done; wait
+}
+
+portDiscovery
